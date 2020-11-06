864e2f0ab8d0d10fabb02a40c8392873bff827ba
RyotaBannai <ryotala0528@gmail.com>
2020-11-07 01:48:38 +0900

    add: implement whole flow with BreakableForEach

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 ### Cold/ Hot producers
 - `Cold`: `データのタイムライン`を Subscribe する時ごとに必ず生成する = データストリームには常に一つのサブスクライバー
 - `Hot`: `データのタイムライン`は全体で一つで、消費者は同じタイムラインに参加することで、データを受け取る(別々の消費者が同時に Subscribe できる) = 過ぎてしまったデータは取得できない.
-- `Cold` を `Hot` にすることもできる.`ConnectableFlowable` `ConnectableObservable`を使う.
-- `ConnectableFlowable` `ConnectableObservable` を `refCount`で `Flowlable`, `Observable`に変換する. 変換する際に, 他の消費者に購読されている限りは途中から購読されても同じタイムライン情で生成されるデータを返す.(つまり, Hot な振る舞いをする)
 
+#### `Cold` -> `Hot` 変換
+- `ConnectableFlowable` `ConnectableObservable`を使う.
+- `refCount`: `ConnectableFlowable` `ConnectableObservable` を `Flowlable`, `Observable`に変換する. 変換する際に, 他の消費者に購読されている限りは途中から購読されても同じタイムライン情で生成されるデータを返す.(つまり, Hot な振る舞いをする)
+- `autoConnect`: `ConnectableFlowable`/ `ConnectableObservable` が引数で指定した数の購読数に達した時に、自動的に subscribe メソッドを呼ぶ. 引数がない場合は、最初に subscribe メソッドが呼ばれたタイミングで処理を開始する. `autoConnect` で生成された, Flowable/ Observable は再度 subscribe を読んでも`再度処理を走ることはない`.
 
- 
+ #### `Hot` -> `Cold` 変換
+ - `publish`: この処理で変換された `ConnectableFlowable`/`ConnectableObservable`は処理を開始した後に購読された場合、それ以降に生成されたデータから新たな消費者に対して通知をする.
+ - `replay`: publish と違って、生成したデータをキャッシュから再度生成して通知する. そのあとは同様な cold の処理。引数がない場合、全てキャッシュから生成し通知。引数がある場合、指定した個数や期間のデータをキャッシュする.
+ - `share`:  `ConnectableFlowable`/`ConnectableObservable` を生成せずに、hot な振る舞いをする Flowable/ Observable を作成する。実施的には, `flowable.publish().refCount()`と同様. 
--- src/main/java/basics/Main.java
+++ src/main/java/basics/Main.java
@@ -1,10 +1,107 @@
 package basics;
 
 import io.reactivex.rxjava3.core.*;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            sample();
+        } catch (Exception e) {
+            System.out.println("Error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void firstTest() throws Exception {
         Flowable<String> flowable = Flowable.just("hello", "world");
         flowable.subscribe(System.out::println);
+    }
+
+    public static void sample() throws Exception {
+        Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> emitter) throws Exception {
+                // String[] array = {"Hello, world", "Good morning, world!"};
+                // List<String> data = Arrays.asList(array); // 固定長の配列を変換しているため add できない. List<String> list = new ArrayList<>(); list.add("test");
+                // data.stream() ...
+                Stream<String> initialStream = Stream.of("Hello, world", "Good morning, world!");
+                BreakableForEach.forEach(initialStream, (elem, breaker) -> {
+                    if (emitter.isCancelled()) { // 購読が解除されたかどうか判定・create を使っている場合は実装者の責任・rx がやってくれるのは通知を止めるだけであって、loop 処理を停止してくれるわけでない.
+                        breaker.stop();
+                    }
+                    emitter.onNext(elem);
+                });
+
+                emitter.onComplete(); // 何らかの処理を行う場合は、onComplete の前にかく。エラーが発生した場合, Subscriber にエラー通知が届かない場合があるため.
+            }
+        }, BackpressureStrategy.BUFFER); // 超過したデータはバッファ.
+
+
+        flowable.observeOn(Schedulers.computation()) // Subscriber の処理を別スレッドで実行.
+                .subscribe(new Subscriber<String>() { // 購読
+                    private Subscription subscription;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        this.subscription = s;
+                        this.subscription.request(1L); // 最初に受け取るデータ数を指定・onSubscribe メソッドの最後にかく
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(String.format("current thread is %s and data is %s", threadName, s));
+
+                        // ここで次に受け取るデータをリクエスト.
+                        this.subscription.request(1L);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(String.format("thread %s is finished.", threadName));
+                    }
+                });
+        Thread.sleep(500L);
+    }
+
+    // ref: https://www.baeldung.com/java-break-stream-foreach
+    private static class BreakableForEach {
+        public static class Breaker {
+            public static boolean shouldBreak = false;
+
+            public void stop() {
+                shouldBreak = true;
+            }
+
+            boolean get() {
+                return shouldBreak;
+            }
+        }
+
+        public static <T> void forEach(Stream<T> stream, BiConsumer<T, Breaker> consumer) {
+            Spliterator<T> spliterator = stream.spliterator(); // spliterator が stream を分けて iterate してくれる関数
+            boolean hadNext = true;
+            Breaker breaker = new Breaker();
+            while (hadNext && !breaker.get()) {
+                hadNext = spliterator.tryAdvance(elem -> {
+                    consumer.accept(elem, breaker);
+                });
+            }
+        }
     }
 }
